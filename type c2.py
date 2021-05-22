1b3417a73f730fa1d0897bbbba9df8b70d7a8f28
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 18:19:26 +0530

    Add files via upload

---
+++ type c2.py
@@ -0,0 +1,10 @@
+a=float(input("Enter Monday's temperature in degree celcius:\n"))
+b=float(input("Enter Tuesday's temperature in degree celcius:\n"))
+c=float(input("Enter Wednesday's temperature in degree celcius:\n"))
+d=float(input("Enter Thrusday's temperature in degree celcius:\n"))
+e=float(input("Enter Friday's temperature in degree celcius:\n"))
+f=float(input("Enter Saturday's temperature in degree celcius:\n"))
+g=float(input("Enter sunday's temperature in degree celcius:\n"))
+h=(a+b+c+d+e+f+g)/7
+print("Average temperature of the week:",h)
+      
