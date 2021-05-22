5b020e98bbb3c4a404e8a21098e32ac8b4417d8e
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 18:48:25 +0530

    Add files via upload

---
+++ type c6.py
@@ -0,0 +1,6 @@
+a=int(input("Enter your first number:\n"))
+b=int(input("Enter your second number:\n"))
+if a%b==0:
+    print(a,"is fully divisible by",b)
+else:
+    print(a,"is not fully divisible by",b)
