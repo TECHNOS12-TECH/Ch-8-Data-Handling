4df21d57c89311b4a13047939394c206a8a3a1ce
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 19:23:59 +0530

    Add files via upload

---
+++ type c10.py
@@ -0,0 +1,9 @@
+y=int(input("Enter number of years:"))
+a=y*365
+b=a*24
+c=b*60
+d=c*60
+print("Total number of Days:",a)
+print("Total number of Hours:",b)
+print("Total number of Minutes:",c)
+print("Total number of seconds:",d)
