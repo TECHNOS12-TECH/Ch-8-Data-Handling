f08d93d258bb22ae9a8f76023654dc90241ae211
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 19:45:36 +0530

    Add files via upload

---
+++ type c12.py
@@ -0,0 +1,6 @@
+import random
+a=random.randint(0,5)
+b=random.randint(0,5)
+print("Random number between 0 and 5(A):",a)
+print("Random number between 0 and 5(B):",b)
+print("A to the power B=",a**b)
