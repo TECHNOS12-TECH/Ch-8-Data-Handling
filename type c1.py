e788d4ab0802986422a92b39f856f96ef3dcd77d
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 18:08:39 +0530

    Add files via upload

---
+++ type c1.py
@@ -0,0 +1,7 @@
+# Type c 1
+p=float(input("enter principal amount:"))
+r=float(input("enter rate of interest:"))
+t=float(input("enter time of interest:"))
+print("SIMPLE INTEREST=",p*r*t/100)
+
+      
