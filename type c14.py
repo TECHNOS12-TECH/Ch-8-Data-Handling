05339870b2e6b3ddc735d68717fc862b58df3c39
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-24 18:29:31 +0530

    Add files via upload

---
+++ type c14.py
@@ -0,0 +1,5 @@
+import random
+a=random.randrange(100,999,5)
+b=random.randrange(100,999,5)
+c=random.randrange(100,999,5)
+print("Three random integers between 100 and 999 which is divisible by 5 are as follows:\n",a,b,c)
