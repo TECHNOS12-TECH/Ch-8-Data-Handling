afed532a55b49eb937f5df624b096b08a65e5c3a
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-24 19:10:16 +0530

    Add files via upload

---
+++ type c21.py
@@ -0,0 +1,5 @@
+# To calculate area of equilateral triangle
+import math
+side=float(input("Enter side of Equilatral trinagle: "))
+area=(math.sqrt(3)/4)*side*side
+print("Area of Equilateral triangle is:",area)
